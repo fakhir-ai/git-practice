9f4126ce032831e880ce9976097a73fe402b26a1
Umar Farooq <mohamed-fakhir.umar-farooq@capgemini.com>
2024-09-24 10:31:08 +0530

    First Python Practice Session

--- git-example.py
+++
@@ -1,2 +0,0 @@
-#Main file for Git
-Main program to execute the process
---
+++ python-session1.py
@@ -0,0 +1,13 @@
+course_fee = 800
+course_rating = 3.5
+course_name = "Python Session 1"
+is_active_course = True
+Empty_Value = None
+
+print("********"*6)
+print(f"course Fee is: {course_fee}")
+print(f"course Rating is: {course_rating}")
+print(f"course Name is: {course_name}")
+print(f"course Validity is: {is_active_course}")
+print(f"course Empty Value is: {Empty_Value}")
+print("********"*6)
