968661a4032f8a888f1fa5f37ac56b5b25fe9f57
Umar Farooq <mohamed-fakhir.umar-farooq@capgemini.com>
2024-09-24 10:39:58 +0530

    To check the type of the datatype

--- python-session1.py
+++ python-session1.py
@@ -5,9 +5,9 @@ is_active_course = True
 Empty_Value = None
 
 print("********"*6)
-print(f"course Fee is: {course_fee}")
-print(f"course Rating is: {course_rating}")
-print(f"course Name is: {course_name}")
-print(f"course Validity is: {is_active_course}")
-print(f"course Empty Value is: {Empty_Value}")
+print(f"Type of course Fee is: {type(course_fee)}")
+print(f"Type of course Rating is: {type(course_rating)}")
+print(f"Type of course Name is: {type(course_name)}")
+print(f"Type of course Validity is: {type(is_active_course)}")
+print(f"Type of course Empty Value is: {type(Empty_Value)}")
 print("********"*6)
