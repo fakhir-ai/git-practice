ef071aa95529d8d3838889a1154d532e16342b51
Umar Farooq <mohamed-fakhir.umar-farooq@capgemini.com>
2024-09-25 11:29:22 +0530

    List Comprehension

--- python-session2.py
+++ python-session2.py
@@ -5,3 +5,6 @@ for order_item in order_amount:
     order_amount_with_gst.append(order_item + order_item * 0.18)
 print(f"Order Amount is : {order_amount}")
 print(f"Order Amount with GST is : {order_amount_with_gst}")
+
+order_gst_short = [oitem+oitem*0.18 for oitem in order_amount]
+print(f"Order Amount with GST List Comprehension : {order_gst_short}")
