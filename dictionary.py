d8a51c11e933f38d1f499ce1e9311bbf49f2e850
Umar Farooq <mohamed-fakhir.umar-farooq@capgemini.com>
2024-10-02 08:27:06 +0530

    nested dictionary joins

--- dictionary.py
+++ dictionary.py
@@ -18,10 +18,27 @@ customer_raw_data = '''cust_id, cust_name, cust_address
 2,def,NJ
 3,ghi,FL
 4,jkl,GE'''
-cust_header = customer_raw_data.split("\n")[0]
+cust_header = customer_raw_data.split("\n")[0].split(",")
 cust_data = customer_raw_data.split("\n")[1:]
 
-cust_dict = {}
-for item in cust_data:
-    cust_dict[item.split(",")[0]] = tuple(item.split(",")[1:])
-print(cust_dict['1'])
+order_raw_data = '''101,10000,1,pending
+102,20000,2,closed
+103,30000,3,processing
+104,40000,4,completed'''
+order_data = order_raw_data.split("\n")
+
+cust_dict = {item.split(",")[0] : tuple(item.split(",")[1:]) for item in cust_data}
+
+nested_dict = {}
+for key, value in cust_dict.items():
+    nested_dict[key]= {
+        cust_header[1] : value[0],
+        cust_header[2] : value[1]
+    }
+print(nested_dict)
+
+combined_data = []
+for orders in order_data:
+    order = orders.split(",")
+    combined_data.append((order[0],order[1],order[2],order[3],nested_dict[order[2]]))
+print(combined_data)
